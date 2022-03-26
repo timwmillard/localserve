fc3c4099794a3af41bf23b06c67e62a9eb8d004e
Tim Millard <tim@rapidpaylegal.com>
2022-03-27 00:37:37 +1100

    Refactor flags and usage

--- main.go
+++ main.go
@@ -9,42 +9,21 @@ import (
 
 func main() {
 
-	var help bool
-	var port string
-	var dir string
-
-	flag.BoolVar(&help, "h", false, "show help")
-	flag.BoolVar(&help, "help", false, "show help")
-
-	flag.StringVar(&port, "p", "8080", "port to serve from")
-	flag.StringVar(&port, "port", "8080", "port to serve from")
-
-	flag.StringVar(&dir, "d", "./", "root directory for server")
-	flag.StringVar(&dir, "directory", ".", "root directory for server")
-
+	port := flag.String("p", "6000", "port to serve from")
+	dir := flag.String("d", ".", "root directory for server")
 	flag.Parse()
 
-	if help {
-		usage()
-		os.Exit(0)
-	}
-
-	http.Handle("/", http.FileServer(http.Dir(dir)))
+	http.Handle("/", http.FileServer(http.Dir(*dir)))
 
 	fmt.Print("Starting local server\n")
-	fmt.Printf("Port: %s\n", port)
-	fmt.Printf("Root directory: %s\n", dir)
+	fmt.Printf("Port: %s\n", *port)
+	fmt.Printf("Root directory: %s\n", *dir)
 	fmt.Println()
 
-	fmt.Printf("Visit \033[34m\033[4mhttp://localhost:%v/\033[m\n", port) // blue color "\033[34m" and underline "\033[4m", then reset "\033[m"
+	fmt.Printf("Visit \033[34m\033[4mhttp://localhost:%v/\033[m\n", *port) // blue color "\033[34m" and underline "\033[4m", then reset "\033[m"
 
-	err := http.ListenAndServe(":"+port, nil)
+	err := http.ListenAndServe(":"+*port, nil)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Could not start server: %v\n", err)
 	}
-
-}
-
-func usage() {
-	fmt.Printf("Usage: %s [-p port] [-d directory]\n", os.Args[0])
 }
